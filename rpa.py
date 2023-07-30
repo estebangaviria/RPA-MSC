d149b4f75c00e602b3c94491ec6a8cdf8681bc49
Esteban Gaviria <93669024+estebangaviria@users.noreply.github.com>
2023-07-29 19:59:32 -0500

    Solving download griky report

--- rpa.py
+++ rpa.py
@@ -7,10 +7,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def rpa_msc(user, psw, url, name):
-
+    error = ''
     #Initial result list:
     results = []
-    results.append(("Name","Step", "Status", "Detail"))
+    #results.append(("Name","Step", "Status", "Detail"))
 
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
@@ -21,30 +21,52 @@ def rpa_msc(user, psw, url, name):
     try:
         driver = webdriver.Chrome()
         wait = WebDriverWait(driver, 50)
+        #results.append((str(name),"1-Initialize Driver", "Successful"))
+    # maneja ValueError
+    except ValueError as ve:
+        error = str(ve)
+    except Exception as e:
+        error = str(e).replace("\n", "")
+    except:
+        error = ''
+
+    if len(error) == 0:
         results.append((str(name),"1-Initialize Driver", "Successful"))
-    except Exception as error:
-        error = str(error).replace("\n", "")
-        results.append((str(name),"1-Initialize Driver", "Unsuccessful:", str(error)))
+    else:
+        results.append((str(name),"1-Initialize Driver", "Unsuccessful", str(error)))
+
+
 
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
 
     # 2-Get Url
+    error = ''
     try:
         driver.get(url)
         driver.maximize_window()
+        #results.append((str(name),"2-Get Url", "Successful"))
+    # maneja ValueError
+    except ValueError as ve:
+        error = str(ve)
+    except Exception as e:
+        error = str(e).replace("\n", "")
+    except:
+        error = ''
+
+    if len(error) == 0:
         results.append((str(name),"2-Get Url", "Successful"))
-    except Exception as error:
-        error = str(error).replace("\n", "")
+    else:
         results.append((str(name),"2-Get Url", "Unsuccessful", str(error)))
+
 
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
 
     # 3-Login
-
+    error = ''
     try:
         # Wait until page loads:
         wait_page = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="login"]/div/input')))
@@ -56,7 +78,7 @@ def rpa_msc(user, psw, url, name):
             # Next buttom:
             driver.find_element(By.XPATH, '//*[@id="main"]/msc-login/div/div[2]/div/form/div/div[2]/msc-button/button').click()
         else:
-            print("La página no se ha cargado completamente.")
+            raise ValueError("La página no se ha cargado completamente.")
 
         # Wait input password:
         imput_password = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]/div/input')))
@@ -67,21 +89,29 @@ def rpa_msc(user, psw, url, name):
             driver.find_element(By.XPATH, '//*[@id="password"]/div/input').send_keys(psw)
             driver.find_element(By.XPATH, '//*[@id="main"]/msc-login/div/div[2]/div/form/div/div[3]/msc-button/button').click()
         else:
-            print("La página no se ha cargado completamente.")
+            raise ValueError("La página no se ha cargado completamente.")
 
+    # maneja ValueError
+    except ValueError as ve:
+        error = str(ve)
+    except Exception as e:
+        error = str(e).replace("\n", "")
+    except:
+        error = ''
+
+    print(f'Debug: The value of error after try-except is {error}')
+
+    if len(error) == 0:
         results.append((str(name),"3-Login", "Successful"))
-
-    except Exception as error:
-        error = str(error).replace("\n", "")
+    else:
         results.append((str(name),"3-Login", "Unsuccessful", str(error)))
-
 
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
 
     #4-Load Statistics section
-
+    error = ''
     try:
         # Wait Statistics:
         wait_statisctics = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/msc-organize/msc-sidebar/ul/li[6]/i')))
@@ -89,7 +119,7 @@ def rpa_msc(user, psw, url, name):
             # Input password:
             driver.find_element(By.XPATH, '//*[@id="main"]/msc-organize/msc-sidebar/ul/li[6]/i').click()
         else:
-            print("La página no se ha cargado completamente.")
+            raise ValueError("La página no se ha cargado completamente.")
 
         # Wait Data Exports:
         wait_data_exports = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/msc-organize/section/msc-organize-statistic/div/msc-common-nav/nav/msc-common-tree/dl/dd[2]/div/div[1]/a')))
@@ -97,14 +127,22 @@ def rpa_msc(user, psw, url, name):
             # Input password:
             driver.find_element(By.XPATH, '//*[@id="main"]/msc-organize/section/msc-organize-statistic/div/msc-common-nav/nav/msc-common-tree/dl/dd[2]/div/div[1]/a').click()
         else:
-            print("La página no se ha cargado completamente.")
+            raise ValueError("La página no se ha cargado completamente.")
 
+        #results.append((str(name),"4-Load Statistics section", "Successful"))
+
+    # maneja ValueError
+    except ValueError as ve:
+        error = str(ve)
+    except Exception as e:
+        error = str(e).replace("\n", "")
+    except:
+        error = ''
+
+    if len(error) == 0:
         results.append((str(name),"4-Load Statistics section", "Successful"))
-
-    except Exception as error:
-        error = str(error).replace("\n", "")
-        results.append((str(name),"4-Load Statistics section", "Successful", str(error)))
-
+    else:
+        results.append((str(name),"4-Load Statistics section", "Unsuccessful", str(error)))
 
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
@@ -113,34 +151,65 @@ def rpa_msc(user, psw, url, name):
 
 
     #5-Download Learner overview report 
-
+    error = ''
     try:
         driver.find_element(By.XPATH, '//*[@id="main"]/msc-organize/section/msc-organize-statistic/div/section/msc-exports/div/div[2]/div/msc-button[1]/button').click()
+        try:
+            # Wait Send Buttom:
+            wait_send_buttom = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-26')))
+        except:
+            pass
 
-        # Wait Send Buttom:
-        wait_send_buttom = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-26')))
         if wait_send_buttom.is_displayed():
             # Input password:
             try:
                 driver.find_element(By.XPATH, '//*[@id="cdk-dialog-0"]/div/div[2]/div[3]/div/msc-button').click()
+                                            #  //*[@id="cdk-dialog-0"]/div/div[2]/div[3]/div/msc-button/button
             except:
-                driver.find_element(By.XPATH, '//*[@id="cdk-dialog-0"]/div/div[2]/div[3]/div/msc-button/button').click()
+                driver.find_element(By.XPATH, '//*[@id="cdk-dialog-0"]/div/div[2]/div[3]/div/msc-button/button').click()  
         else:
-            print("La página no se ha cargado completamente.")
+            # Wait Send Buttom GRIKY:
+            wait_send_buttom_griky = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-29')))
+            if  wait_send_buttom_griky.is_displayed():
+                # Input password:
+                try:
+                    driver.find_element(By.XPATH, '//*[@id="cdk-dialog-0"]/div/div[2]/div[3]/div/msc-button').click()
+                                                #  //*[@id="cdk-dialog-0"]/div/div[2]/div[3]/div/msc-button/button
+                except:
+                    driver.find_element(By.XPATH, '//*[@id="cdk-dialog-0"]/div/div[2]/div[3]/div/msc-button/button').click()  
+            else:
+                raise ValueError("La página no se ha cargado completamente.")
+            
+        try:
+            # Wait ok Buttom:
+            wait_ok_buttom = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-27')))
+        except:
+            pass
 
-        # Wait ok Buttom:
-        wait_ok_buttom = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-27')))
         if wait_ok_buttom.is_displayed():
             # Input password:
             driver.find_element(By.XPATH, '//*[@id="cdk-dialog-1"]/div/div[2]/div/msc-button/button').click()
         else:
-            print("La página no se ha cargado completamente.")
+            # Wait ok Buttom GRIKY:
+            wait_ok_buttom_griky = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-30')))
+            if wait_ok_buttom_griky.is_displayed():
+                # Input password:
+                driver.find_element(By.XPATH, '//*[@id="cdk-dialog-1"]/div/div[2]/div/msc-button/button').click()
+            else:
+                raise ValueError("La página no se ha cargado completamente.")            
+        
+    # maneja ValueError
+    except ValueError as ve:
+        error = str(ve)
+    except Exception as e:
+        error = str(e).replace("\n", "")
+    except:
+        error = ''
 
+    if len(error) == 0:
         results.append((str(name),"5-Download Learner overview report", "Successful"))
-
-    except Exception as error:
-        error = str(error).replace("\n", "")
-        results.append((str(name),"5-Download Learner overview report", "Successful", str(error)))
+    else:
+        results.append((str(name),"5-Download Learner overview report", "Unsuccessful", str(error)))
 
 
     ###-------------------------------------------------------------###
@@ -151,37 +220,63 @@ def rpa_msc(user, psw, url, name):
 
 
     #6-Download Learner engagement report 
-
+    error = ''
     try:
-        
         try:
             driver.find_element(By.XPATH, '//*[@id="main"]/msc-organize/section/msc-organize-statistic/div/section/msc-exports/div/div[3]/div/msc-button[1]/button').click()
         except:
             driver.find_element(By.XPATH, '//*[@id="cdk-dialog-0"]/div/div[2]/div[3]/div/msc-button/button').click()
 
+        try:
+            # Wait Send Buttom:
+            wait_send_buttom = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-28')))
+        except:
+            pass
 
-        # Wait Send Buttom:
-        wait_send_buttom = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-28')))
         if wait_send_buttom.is_displayed():
             # Input password:
             driver.find_element(By.XPATH, '//*[@id="cdk-dialog-2"]/div/div[2]/div[3]/div/msc-button/button').click()
         else:
-            print("La página no se ha cargado completamente.")
+            # Wait Send Buttom GRIKY:
+            wait_send_buttom_griky = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-31')))
+            if wait_send_buttom_griky.is_displayed():
+                # Input password:
+                driver.find_element(By.XPATH, '//*[@id="cdk-dialog-2"]/div/div[2]/div[3]/div/msc-button/button').click()
+            else:
+                raise ValueError("La página no se ha cargado completamente.")            
 
-        # Wait ok Buttom:
-        wait_ok_buttom = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-30')))
+        try:
+            # Wait ok Buttom:
+            wait_ok_buttom = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-30')))
+        except:
+            pass
+        
         if wait_ok_buttom.is_displayed():
             # Input password:
             driver.find_element(By.XPATH, '//*[@id="cdk-dialog-3"]/div/div[2]/div/msc-button/button').click()
         else:
-            print("La página no se ha cargado completamente.")
+            # Wait ok Buttom GRIKY:
+            wait_ok_buttom_griky = wait.until(EC.presence_of_element_located((By.ID, 'cdk-overlay-33')))
+            if wait_ok_buttom_griky.is_displayed():
+                # Input password:
+                driver.find_element(By.XPATH, '//*[@id="cdk-dialog-3"]/div/div[2]/div/msc-button/button').click()
+            else:
+                raise ValueError("La página no se ha cargado completamente.")            
 
+        #results.append((str(name),"6-Download Learner engagement report", "Successful"))
+
+    # maneja ValueError
+    except ValueError as ve:
+        error = str(ve)
+    except Exception as e:
+        error = str(e).replace("\n", "")
+    except:
+        error = ''
+
+    if len(error) == 0:
         results.append((str(name),"6-Download Learner engagement report", "Successful"))
-
-    except Exception as error:
-        error = str(error).replace("\n", "")
-        results.append((str(name),"6-Download Learner engagement report", "Successful", str(error)))
-
+    else:
+        results.append((str(name),"6-Download Learner engagement report", "Unsuccessful", str(error)))
 
     ###-------------------------------------------------------------###
     ###-------------------------------------------------------------###
@@ -190,23 +285,9 @@ def rpa_msc(user, psw, url, name):
     # Añadir una pausa y Cerrar el driver de Selenium
     time.sleep(2)
 
-    # Make results file:
-    for i in results:
-        print(i)
-
-    with open("results.csv", "w") as archivo:
-       
-        for row in results:
-            # Comprobar la longitud de la fila
-            if len(row) == 4:
-                name, step, status, detail = row
-                archivo.write(name + "," + step + "," + status + "," + detail + "\n")
-            elif len(row) == 3:
-                name, step, status = row
-                archivo.write(name + "," + step + "," + status + "\n")
-
-
     driver.quit()
+
+    return results
 
 
 ###-------------------------------------------------------------###
